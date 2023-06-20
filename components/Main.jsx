7341782835e2bbf22b1af1e4d2989e1cc1b48b7f
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 12:50:12 -0700

    updated /components/Main with content

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,14 @@
 export default function Main() {
     return (
-        <h1>Main goes here</h1>
+        <main>
+            <h1>Facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
