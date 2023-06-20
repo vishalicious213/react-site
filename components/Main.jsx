a8724cbfe34e5081de6593744c551bacd609a533
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 13:01:48 -0700

    styled /components/Main

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,8 +1,8 @@
 export default function Main() {
     return (
         <main>
-            <h1>Facts about React</h1>
-            <ul>
+            <h1 className="main-title">Facts about React</h1>
+            <ul className="main-facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
