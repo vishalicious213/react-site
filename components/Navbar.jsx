3e48c8c099dad8287da997fe85a63f3ce69266b7
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 12:41:27 -0700

    updated /components/Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,13 @@
+import logo from "../public/react.svg"
+
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <div className="title">
+                <img src={logo} />
+                <h3 className="name">ReactFacts</h3>
+            </div>
+            <h4 className="title">React Course - Project 1</h4>
+        </nav>
     )
 }
