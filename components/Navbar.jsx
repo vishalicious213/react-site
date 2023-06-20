287287acc36ca811428e3003c19076e5c37b2ffd
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 14:24:59 -0700

    updated Navbar & Main to 550px max-width

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,11 +3,13 @@ import logo from "/react.svg"
 export default function Navbar() {
     return (
         <nav>
-            <div className="title">
-                <img src={logo} />
-                <h3 className="name">ReactFacts</h3>
+            <div className="nav-container">
+                <div className="title">
+                    <img src={logo} />
+                    <h3 className="name">ReactFacts</h3>
+                </div>
+                <h4 className="title">React Course - Project 1</h4>
             </div>
-            <h4 className="title">React Course - Project 1</h4>
         </nav>
     )
 }
