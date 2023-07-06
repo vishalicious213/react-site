621085938800e801965efeec0fe5577474bdf906
vishalicious213 <vishalicious213@gmail.com>
2023-07-05 17:56:38 -0700

    adding lightmode prop to App for Main & Navbar

--- App.jsx
+++ App.jsx
@@ -1,13 +1,14 @@
+import { useState } from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+    const [lightMode, setLightMode] = useState(false)
+
     return (
         <>
-            <Navbar />
-            <Main />
-            {/* <Navbar lightMode="light" /> */}
-            {/* <Main lightMode="light" /> */}
+            <Navbar lightMode={lightMode} />
+            <Main lightMode={lightMode} />
         </>
     )
 }
