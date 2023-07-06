8a8463ad3fadfc0b70586b82a2c39d36aed1fa37
vishalicious213 <vishalicious213@gmail.com>
2023-07-05 18:46:03 -0700

    toggler text changes color with mode

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,11 +10,11 @@ export default function Navbar(props) {
                 </div>
 
                 <div className="toggler" onClick={props.toggleLightMode}>
-                    <p className="toggler-light">Light</p>
+                    <p className={props.lightMode ? "toggler-dark" : "toggler-light"}>Light</p>
                     <div className={props.lightMode ? "toggler-slider toggler-slider-light" : "toggler-slider"}>
                         <div className="toggler-slider-circle"></div>
                     </div>
-                    <p className="toggler-dark">Dark</p>
+                    <p className={props.lightMode ? "toggler-light" : "toggler-dark"}>Dark</p>
                 </div>
 
                 {/* <h4 className="title" onClick={props.toggleLightMode}>React Course - Project 1</h4> */}
