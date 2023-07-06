fa916799085f4cd5190a1eaa4ce4f4ef09812946
vishalicious213 <vishalicious213@gmail.com>
2023-07-05 18:08:59 -0700

    toggling Main text colors

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,9 +1,9 @@
 export default function Main(props) {
     return (
-        <main className={props.lightMode ? "light" : ""}>
+        <main className={props.lightMode ? "light light-text" : ""}>
             <div className="main-container">
-                <h1 className="main-title">Facts about React</h1>
-                <ul className="main-facts">
+                <h1 className={props.lightMode ? "main-title light-text" : "main-title"}>Facts about React</h1>
+                <ul className={props.lightMode ? "main-facts light-text" : "main-facts"}>
                     <li>Was first released in 2013</li>
                     <li>Was originally created by Jordan Walke</li>
                     <li>Has well over 100K stars on GitHub</li>
